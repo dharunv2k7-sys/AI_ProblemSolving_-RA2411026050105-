4de02bf7d66054a30a75419fc04d2acc00fcc1f2
Dharun Venkat SR <dharunv2k7@gmail.com>
2026-04-30 00:25:48 +0530

    Add BFS algorithm and example graph
    Implement breadth-first search algorithm to find a path in a graph.

---
+++ bfs.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+def bfs(graph, start, goal):
+    visited = set()
+    queue = deque([[start]])
+
+    while queue:
+        path = queue.popleft()
+        node = path[-1]
+
+        if node == goal:
+            return path
+
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph[node]:
+                new_path = list(path)
+                new_path.append(neighbor)
+                queue.append(new_path)
+
+    return None
+
+
+def get_path():
+    graph = {
+        'A': ['B', 'C'],
+        'B': ['D', 'E'],
+        'C': ['F'],
+        'D': [],
+        'E': ['F'],
+        'F': []
+    }
+    return bfs(graph, 'A', 'F')
